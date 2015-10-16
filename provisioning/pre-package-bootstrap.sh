5ffeec112b4ef995e654d6ed9b70e680cd79e92e
Jason Yeo <jasonyeo88@gmail.com>
2015-10-16 15:26:33 +0800

    Use a fixed ntp server. Use apt mirror method.

--- provisioning/pre-package-bootstrap.sh
+++ provisioning/pre-package-bootstrap.sh
@@ -1,3 +1,6 @@
+mv /home/vagrant/ntp.conf /etc/ntp.conf
+service ntp restart
+mv /home/vagrant/sources.list /etc/apt/sources.list
 apt-get update
 apt-get install -y git subversion
 apt-get install -y maven2 gradle npm
