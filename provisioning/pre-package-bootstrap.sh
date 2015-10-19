d42f3b6a3416307efd28d3f9f64b1484176e37e8
Jason Yeo <jasonyeo88@gmail.com>
2015-10-19 11:40:59 +0800

    Use ruby binaries instead of compiling them

--- provisioning/pre-package-bootstrap.sh
+++ provisioning/pre-package-bootstrap.sh
@@ -2,6 +2,8 @@ mv /home/vagrant/ntp.conf /etc/ntp.conf
 service ntp restart
 mv /home/vagrant/sources.list /etc/apt/sources.list
 apt-get update
-apt-get install -y git subversion
-apt-get install -y maven2 gradle npm
-apt-get install -y curl rdiff-backup zip
+apt-get install -y python-software-properties
+apt-add-repository ppa:brightbox/ruby-ng
+apt-get update
+apt-get install -y git subversion maven2 gradle npm curl rdiff-backup zip ruby2.2-dev ruby-switch
+ruby-switch --set ruby2.2
