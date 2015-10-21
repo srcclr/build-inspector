7d4607fa926dbf083945559223d464ae6ebefdc6
Jason Yeo <jasonyeo88@gmail.com>
2015-10-22 04:54:05 +0800

    Add echos to indicate where we are at

--- provisioning/pre-package-bootstrap.sh
+++ provisioning/pre-package-bootstrap.sh
@@ -1,12 +1,17 @@
 mv /home/vagrant/sources.list /etc/apt/sources.list
 service puppet stop
 service chef-client stop
+echo "Adding brightbox's ruby repository"
 apt-add-repository ppa:brightbox/ruby-ng
+echo "Updating apt"
 apt-get update
+echo "Installing dependencies"
 apt-get install -y build-essential git-core zlib1g-dev libssl-dev \
   libreadline-dev libyaml-dev subversion maven2 gradle npm curl rdiff-backup \
   zip ruby2.2 ruby2.2-dev ruby-switch libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev \
   libcurl4-openssl-dev libffi-dev openjdk-7-jdk
+echo "Switching to ruby2.2"
 ruby-switch --set ruby2.2
 echo "gem: --no-ri --no-rdoc" >> /etc/gemrc
+echo "Installing bundler"
 gem install bundler
