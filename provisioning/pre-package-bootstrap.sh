2ff27b47a451ad336527b57eba762c698801185a
Jason Yeo <jasonyeo88@gmail.com>
2015-10-22 04:53:13 +0800

    Add openjdk

--- provisioning/pre-package-bootstrap.sh
+++ provisioning/pre-package-bootstrap.sh
@@ -6,7 +6,7 @@ apt-get update
 apt-get install -y build-essential git-core zlib1g-dev libssl-dev \
   libreadline-dev libyaml-dev subversion maven2 gradle npm curl rdiff-backup \
   zip ruby2.2 ruby2.2-dev ruby-switch libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev \
-  libcurl4-openssl-dev libffi-dev
+  libcurl4-openssl-dev libffi-dev openjdk-7-jdk
 ruby-switch --set ruby2.2
 echo "gem: --no-ri --no-rdoc" >> /etc/gemrc
 gem install bundler
