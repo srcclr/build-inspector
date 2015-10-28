ffdec1d93513d98e1e06964aeee9430385bf3c96
Jason Yeo <jasonyeo88@gmail.com>
2015-10-28 11:17:11 +0800

    Use chris-lea's nodejs repository The ubuntu's official one is wonky

--- provisioning/pre-package-bootstrap.sh
+++ provisioning/pre-package-bootstrap.sh
@@ -7,11 +7,13 @@ service puppet stop
 service chef-client stop
 echo "Adding brightbox's ruby repository"
 apt-add-repository ppa:brightbox/ruby-ng
+echo "Adding chis-lea's node js repository"
+add-apt-repository ppa:chris-lea/node.js
 echo "Updating apt"
 apt-get update
 echo "Installing dependencies"
 apt-get install -y build-essential git-core zlib1g-dev libssl-dev \
-  libreadline-dev libyaml-dev subversion maven2 gradle npm curl rdiff-backup \
+  libreadline-dev libyaml-dev subversion maven2 gradle nodejs rdiff-backup \
   zip ruby2.2 ruby2.2-dev ruby-switch libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev \
   libcurl4-openssl-dev libffi-dev openjdk-7-jdk
 echo "Switching to ruby2.2"
