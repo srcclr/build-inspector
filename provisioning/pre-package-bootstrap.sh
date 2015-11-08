0b5b16b089c8f9f04660a6d934264f79a9c07c80
Jason Yeo <jasonyeo88@gmail.com>
2015-11-09 03:22:51 +0800

    Remove environment file from provisioning so that we can get the right home dir

--- provisioning/pre-package-bootstrap.sh
+++ provisioning/pre-package-bootstrap.sh
@@ -1,20 +1,28 @@
 set -e
-mv /home/vagrant/sources.list /etc/apt/sources.list
-mv /home/vagrant/environment /etc/environment
-mv /home/vagrant/sshd /etc/pam.d/sshd
+mv ~/sources.list /etc/apt/sources.list
+mv ~/sshd /etc/pam.d/sshd
+
+echo 'GEM_HOME="/home/vagrant/.gem"' >> /etc/environment
+echo 'PATH="$HOME/.gem/ruby/2.2.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"' >> /etc/enviroment
+
 echo "Stopping puppet and chef"
 service puppet stop
 service chef-client stop
+
 echo "Adding brightbox's ruby repository"
 apt-add-repository ppa:brightbox/ruby-ng
+
 echo "Adding chis-lea's node js repository"
 add-apt-repository ppa:chris-lea/node.js
+
 echo "Updating apt"
 apt-get update
+
 echo "Installing dependencies"
 apt-get install -y build-essential git-core zlib1g-dev libssl-dev \
   libreadline-dev libyaml-dev subversion maven2 gradle nodejs rdiff-backup \
   zip ruby2.2 ruby2.2-dev ruby-switch libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev \
   libcurl4-openssl-dev libffi-dev openjdk-7-jdk
+
 echo "Switching to ruby2.2"
 ruby-switch --set ruby2.2
