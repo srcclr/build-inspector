a34316c93f9f00219dbe90aa210f65609e2d4127
Jason Yeo <jasonyeo88@gmail.com>
2015-11-09 05:08:17 +0800

    Poll for new processes more frequently

--- get_processes_job.rb
+++ get_processes_job.rb
@@ -7,11 +7,10 @@ class GetProcessesJob
   def start
     old_value = ProcTable.ps.map(&:cmdline)
     new_value = []
-    every(0.01) do
+    every(0.001) do
       new_value = ProcTable.ps.map(&:cmdline)
       if !((new_value - old_value).empty?)
         File.open("/evidence/ps/#{Time.now.strftime('%Y%m%d%H%M%S')}-ps", 'w') do |f|
-          puts 'Writing ...'
           f.write((new_value - old_value).to_yaml)
         end
         old_value = new_value
